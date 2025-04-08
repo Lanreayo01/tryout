1402b9eb71c9742b1c6db6d38b1c28a918261f32
Lanreayo01 <alfreddacosta75@gmail.com>
2025-04-08 11:25:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let age;
+age = 19;
+console.log(age);
+const minimumAge = 16;
+let currentYear = 2025;
+yearOfBirth = currentYear - age;
+console.log('Year of Birth is', yearOfBirth); 
+differenceFromEligibility = age - minimumAge;
+console.log('Year difference from eligibility is', differenceFromEligibility)
